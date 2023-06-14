f06437f0e2c3c203b3941bc86ca1a236d45e1613
Lê Hoàng Long <lelong.ll32@gmail.com>
2023-06-14 13:39:09 +0700

    Fix bug entry point account

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\user\Account::class,
+            'model' => App\Models\Account::class,
         ],
 
         // 'users' => [
